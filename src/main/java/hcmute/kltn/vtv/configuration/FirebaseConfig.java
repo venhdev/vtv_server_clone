e040bb90c771da3b20109ae11431871369022f01
ToDuyVuong <comc5288@gmail.com>
2024-03-12 00:35:38 +0700

    Add IImageService

--- src/main/java/hcmute/kltn/vtv/configuration/FirebaseConfig.java
+++ src/main/java/hcmute/kltn/vtv/configuration/FirebaseConfig.java
@@ -3,6 +3,7 @@ package hcmute.kltn.vtv.configuration;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.StorageClient;
 import com.google.firebase.messaging.FirebaseMessaging;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
@@ -11,7 +12,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-
+import com.google.common.base.Strings;
+import com.google.cloud.storage.Bucket;
 import java.io.IOException;
 
 /**
@@ -25,9 +27,14 @@ public class FirebaseConfig {
     @Value("${firebase-configuration-file}")
     private String firebaseConfigPath;
 
-    /**
-     * Initializes the Firebase app if it doesn't already exist.
-     */
+
+    @Value("${firebase-image-configuration-file}")
+    private String firebaseImageConfigPath;
+
+    @Value("image-vtv.appspot.com")
+    private String bucketName;
+
+
     @PostConstruct
     public void initializeFirebaseApp() {
         if (FirebaseApp.getApps().isEmpty()) {
@@ -51,5 +58,32 @@ public class FirebaseConfig {
     public FirebaseMessaging firebaseMessaging() {
         return FirebaseMessaging.getInstance();
     }
+
+    @Bean
+    public FirebaseApp imageFirebaseApp() throws IOException {
+        String appName = "imageFirebaseApp";
+
+        if (FirebaseApp.getApps().stream().noneMatch(app -> app.getName().equals(appName))) {
+            try {
+                FirebaseOptions options = new FirebaseOptions.Builder()
+                        .setCredentials(GoogleCredentials.fromStream(
+                                new ClassPathResource(firebaseImageConfigPath).getInputStream()))
+                        .build();
+                FirebaseApp.initializeApp(options, appName);
+                logger.info("Firebase app '{}' has been initialized.", appName);
+            } catch (IOException e) {
+                logger.error("Failed to initialize Firebase app:", e);
+                throw e; // Propagate the exception
+            }
+        }
+
+        return FirebaseApp.getInstance(appName);
+    }
+
+    @Bean
+    public Bucket storageClient(FirebaseApp imageFirebaseApp) {
+        return StorageClient.getInstance(imageFirebaseApp).bucket(bucketName);
+    }
+
 }
 
