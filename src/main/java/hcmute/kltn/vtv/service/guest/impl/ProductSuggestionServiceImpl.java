726bffe4d9492f3d90683705600e88dc222958e4
ToDuyVuong <comc5288@gmail.com>
2024-04-10 23:16:30 +0700

    fix getProductSuggestionByRandomlyAndProductId

--- src/main/java/hcmute/kltn/vtv/service/guest/impl/ProductSuggestionServiceImpl.java
+++ src/main/java/hcmute/kltn/vtv/service/guest/impl/ProductSuggestionServiceImpl.java
@@ -8,6 +8,7 @@ import hcmute.kltn.vtv.model.extra.Status;
 import hcmute.kltn.vtv.repository.vtv.CategoryRepository;
 import hcmute.kltn.vtv.repository.vendor.ProductRepository;
 import hcmute.kltn.vtv.service.guest.IProductSuggestionService;
+import hcmute.kltn.vtv.util.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -38,21 +39,21 @@ public class ProductSuggestionServiceImpl implements IProductSuggestionService {
     public ListProductPageResponse suggestByRandomlyAndProductId(Long productId, int page, int size, boolean inShop) {
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new RuntimeException("Không tìm thấy sản phẩm phù hợp"));
-        String content = product.getName() + " " + product.getDescription();
-        Page<Product> productPage = new PageImpl<>(new ArrayList<>());
+        String content = product.getName();
+        Page<Product> productPage;
         String message;
 
         if (inShop) {
             productPage = productRepository.searchFullTextOnShopByNameAndStatusAndSortRandomly(
                             content, product.getShop().getShopId(), Status.ACTIVE.toString(),
                             PageRequest.of(page - 1, size))
-                    .orElseThrow(() -> new RuntimeException("Không tìm thấy sản phẩm phù hợp"));
+                    .orElseThrow(() -> new NotFoundException("Không tìm thấy sản phẩm phù hợp"));
             message = "Lấy danh sách sản phẩm gợi ý trong cùng cửa hàng thành công!";
         } else {
             productPage = productRepository.searchFullTextByNameAndStatusAndSortRandomly(
                             content, Status.ACTIVE.toString(),
                             PageRequest.of(page - 1, size))
-                    .orElseThrow(() -> new RuntimeException("Không tìm thấy sản phẩm phù hợp"));
+                    .orElseThrow(() -> new NotFoundException("Không tìm thấy sản phẩm phù hợp"));
             message = "Lấy danh sách sản phẩm gợi ý thành công!";
         }
 
