68bbfdea3c5c3f1e536024d44266e9003ebee5a7
ToDuyVuong <comc5288@gmail.com>
2024-04-10 23:24:01 +0700

    fix ListOrderResponse

--- src/main/java/hcmute/kltn/vtv/model/data/user/response/ListOrderResponse.java
+++ src/main/java/hcmute/kltn/vtv/model/data/user/response/ListOrderResponse.java
@@ -16,16 +16,22 @@ public class ListOrderResponse extends ResponseAbstract {
 
     private int count;
 
+    private Long totalPayment;
+
+    private Long totalPrice;
+
     private List<OrderDTO> orderDTOs;
 
 
     public static ListOrderResponse listOrderResponse(List<Order> orders, String message, String status) {
         ListOrderResponse response = new ListOrderResponse();
         response.setOrderDTOs(OrderDTO.convertEntitiesToDTOs(orders));
+        response.setTotalPayment(orders.stream().mapToLong(Order::getPaymentTotal).sum());
+        response.setTotalPrice(orders.stream().mapToLong(Order::getTotalPrice).sum());
+        response.setCount(orders.size());
         response.setMessage(message);
         response.setStatus(status);
         response.setCode(200);
-        response.setCount(orders.size());
         return response;
     }
 
