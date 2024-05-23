7a7c979384a169f04f9326a02f324d9d1fda04bb
ToDuyVuong <comc5288@gmail.com>
2024-05-23 14:56:45 +0700

    test token v3

--- src/main/java/hcmute/kltn/vtv/configuration/WebSocketConfig.java
+++ src/main/java/hcmute/kltn/vtv/configuration/WebSocketConfig.java
@@ -1,10 +1,16 @@
 package hcmute.kltn.vtv.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import java.security.Principal;
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -17,9 +23,19 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         config.setUserDestinationPrefix("/room");
     }
 
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp");
+        registry.addEndpoint("/ws-stomp").setHandshakeHandler(new DefaultHandshakeHandler() {
+            @Override
+            protected Principal determineUser(org.springframework.http.server.ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
+                String token = (String) attributes.get("token");
+                return () -> token;
+            }
+        }).withSockJS();
     }
+
+
+
 }
 
